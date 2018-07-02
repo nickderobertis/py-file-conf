90081128ee0a842e7ce1cc716f650aefc6a5145f
Nick DeRobertis <whoopnip@gmail.com>
2018-07-02 10:47:09 -0400

    Better whitespace handling. Strip whitespace inbetween imports and assigns, but leave whitespace in imports and in assigns

--- dero/manager/config/logic/load.py
+++ dero/manager/config/logic/load.py
@@ -1,4 +1,5 @@
 from typing import Tuple, List, Union
+from copy import deepcopy
 
 ListOfStrs = List[str]
 TwoNoneTuple = Tuple[None, None]
@@ -11,35 +12,29 @@ def _split_lines_into_import_and_assignment(lines: ListOfStrs, strip_lines=True)
     import_section = []
     assignment_section = []
     # We can have three cases for the following two booleans:
-    # The file starts in the import section (in_import_section=True, in_assignment_section=False).
-    # Then, we enter the whitespace section between import and assignments.
-    # This will be ignored (in_import_section=False, in_assignment_section=False)
+    # The file starts in the import section
+    # Then, we enter the whitespace section between import and assignments. Still consider this the import section.
     # Finally, once text is detected again after import section,
-    # assignment section starts (in_import_section=False, in_assignment_section=True)
+    # assignment section starts (in_import_section=False)
     in_import_section = True
-    in_assignment_section = False
-    for line in lines:
+    for i, line in enumerate(lines):
 
         if strip_lines:
             line = line.strip()
 
-        # Handle import section
+        # Handle import section and add whitespace below import section
         if in_import_section:
-            if 'import' in line:
+            if 'import' in line or _is_whitespace_line(line):
                 import_section.append(line)
             else:
-                in_import_section = False  # no longer dealing with imports
-
-        # Handle whitespace section in between import and assignment, and switching to assignment
-        if not in_import_section and not in_assignment_section:
-            if _is_whitespace_line(line):
-                continue # skip whitespace inbetween import and assignment, but not between assignments or between imports
-            else: # finished import section, found something other than whitespace. Must be in assignment section
-                in_assignment_section = True
+                in_import_section = False  # no longer dealing with imports, must have assignment line
 
         # Assignment section, just output
-        if in_assignment_section:
+        if not in_import_section:
             assignment_section.append(line)
+
+    # Import section was handled as both import section and following whitespace. Strip following whitespace.
+    import_section = _strip_whitespace_lines_from_end_of_lines(import_section)
 
     return import_section, assignment_section
 
@@ -54,3 +49,24 @@ def _split_assignment_line_into_variable_name_and_assignment(line: str) -> TwoTu
 
 def _is_whitespace_line(line: str) -> bool:
     return line.strip() == ''
+
+def _strip_whitespace_lines_from_end_of_lines(lines: List[str]) -> List[str]:
+
+    # Handle empty list
+    if lines == []:
+        return []
+
+    reversed_lines = deepcopy(lines)
+    reversed_lines.reverse()
+
+    # starting from end first. Discard all lines until non-whitespace is found
+    for line_index, line in enumerate(reversed_lines):
+        if not _is_whitespace_line(line):
+            break
+
+    output_lines = reversed_lines[line_index:]
+    output_lines.reverse() # back to first first
+
+    return output_lines
+
+
