8d6eec38b96940fae3d450698450ef6408673cd6
Nick DeRobertis <whoopnip@gmail.com>
2018-08-19 15:54:29 -0400

    Replace cleanup_kwargs for DataPipeline with dict() constructor if None in config file

--- dero/manager/pipelines/models/file.py
+++ dero/manager/pipelines/models/file.py
@@ -1,7 +1,11 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from dero.manager.basemodels.config import ConfigBase
 
 from dero.manager.basemodels.file import ConfigFileBase
 from dero.manager.imports.models.statements.obj import ObjectImportStatement
 from dero.manager.assignments.models.statement import AssignmentStatement
+from dero.manager.io.file.write.asttosource import ast_node_to_source
 
 class FunctionConfigFile(ConfigFileBase):
     """
@@ -14,9 +18,32 @@ class FunctionConfigFile(ConfigFileBase):
     ]
 
     # assignment lines to always include at beginning. pass assign objects
-    always_assigns = [AssignmentStatement.from_str('s = Selector()', preferred_position='begin')]
+    always_assigns = [
+        AssignmentStatement.from_str('s = Selector()', preferred_position='begin'),
+    ]
+
+    # always assign dict, where assigns will get added if item name matches dict key
+    always_assign_with_names_dict = {
+        'DataPipeline': [
+            AssignmentStatement.from_str('cleanup_kwargs = dict(\n    \n)', preferred_position='end')
+        ]
+    }
 
     # class to use for interfacing with file
     # no need to override default
     # interface_class = ConfigFileInterface
 
+    def save(self, config: 'ConfigBase'):
+        # If empty cleanup kwargs on DataPipeline, then delete so can be replaced with dict() constructor
+        # from always assigns
+        if _should_replace_cleanup_kwargs(self, config):
+            config.pop('cleanup_kwargs')
+
+        super().save(config)
+
+
+def _should_replace_cleanup_kwargs(config_file: FunctionConfigFile, config: 'ConfigBase') -> bool:
+    return all([
+        config_file.name == 'DataPipeline',
+        'cleanup_kwargs' in config
+    ]) and ast_node_to_source(config['cleanup_kwargs']) == 'None\n'
