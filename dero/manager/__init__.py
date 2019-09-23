a20355d21cf07792278bc34da01503c66a56fffe
Nick DeRobertis <whoopnip@gmail.com>
2019-09-22 20:18:11 -0400

    Add docstrings for packages

--- dero/manager/__init__.py
+++ dero/manager/__init__.py
@@ -1,10 +1,10 @@
 
 """
-Pipeline Manager is a Python framework for flow-based programming and managing configuration. To use it, you register
+Flow Manager is a Python framework for flow-based programming and managing configuration. To use it, you register
 your main functions, and config files are created automatically for them. It provides a way to run these functions
 individually, in a list, in sections, or a combination thereof. Configuration has multiple layers, with the defaults
 for the function being the lowest level config, which then gets overrided by project config, which gets overrided by
-high-level section configs, then lower-level section configs, and finally by any local config updates in a script.
+the config file, and finally by any local config updates in a script.
 """
 
 from dero.manager.main import PipelineManager
