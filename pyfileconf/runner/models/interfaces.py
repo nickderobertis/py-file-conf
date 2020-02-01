99221a08b193ad5b407ec1da848889ab1b5d1024
Nick DeRobertis <whoopnip@gmail.com>
2020-02-01 14:34:16 -0500

    Fix circular import issue with importing itemview into runner interfaces

--- pyfileconf/runner/models/interfaces.py
+++ pyfileconf/runner/models/interfaces.py
@@ -1,11 +1,11 @@
-from typing import Union, List, Any
-
-from pyfileconf.selector.models.itemview import ItemView
+from typing import Union, List, Any, TYPE_CHECKING
+if TYPE_CHECKING:
+    from pyfileconf.selector.models.itemview import ItemView
 
 ListOfStrs = List[str]
 StrOrListOfStrs = Union[str, ListOfStrs]
 Result = Any
 Results = List[Result]
 ResultOrResults = Union[Result, Results]
-StrOrView = Union[str, ItemView]
-RunnerArgs = Union[str, List[str], ItemView, List[ItemView]]
+StrOrView = Union[str, 'ItemView']
+RunnerArgs = Union[str, List[str], 'ItemView', List['ItemView']]
