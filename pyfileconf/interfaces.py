905cba73caad2a302a770d73508a5e59936a1a82
Nick DeRobertis <whoopnip@gmail.com>
2020-07-20 05:42:22 -0400

    Restructure SpecificClassConfigDict into typed dict

---
+++ pyfileconf/interfaces.py
@@ -0,0 +1,16 @@
+from typing import Type, List
+
+from typing_extensions import TypedDict
+
+
+SpecificClassConfigDict = TypedDict(
+    "SpecificClassConfigDict",
+    {
+        "name": str,
+        "class": Type,
+        "always_import_strs": List[str],
+        "always_assign_strs": List[str],
+        "key_attr": str,
+    },
+    total=False
+)
